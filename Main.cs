7e3bacec509f4015411219a56ca5b3c0f9847c77
uditaren <uditaren@bitsetter.de>
2012-02-17 23:42:45 +0100

    status

--- Main.cs
+++ Main.cs
@@ -1,5 +1,9 @@
 using System;
-using Gtk;
+
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ScubyNet
 {
@@ -7,10 +11,22 @@ namespace ScubyNet
 	{
 		public static void Main (string[] args)
 		{
-			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
-			Application.Run ();
+			//Application.Init ();
+			//MainWindow win = new MainWindow ();
+			//win.Show ();
+			//Application.Run ();
+			Console.WriteLine("test");
+			
+			Socket 	s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			s.Connect("test.scubywars.de", 1337);
+			byte[] buf = new byte[1024];
+			int ret = s.Receive(buf);
+			string sret = System.Text.Encoding.ASCII.GetString(buf, 0, ret);
+			Console.WriteLine(sret);
+			
+			
+			
+			
 		}
 	}
 }
