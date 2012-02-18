6ea8b98986f752c260db7d586037a4f4a05574b8
Alexander Herold <foxplus@gmx.net>
2012-02-18 02:57:37 +0100

    PackWorld.cs hinzugefügt

---
+++ PackWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ScubyNet
+{
+    class PackWorld : Packet
+    {
+        public PackWorld() { }
+
+        private int miEntityCount;
+        
+        protected override Packet createFromData(ref byte[] rbData)
+        {
+            //throw new NotImplementedException ();
+            PackWorld oRet = new PackWorld();
+            miEntityCount = readIntFrom(ref rbData, 0);
+            return oRet;
+        }
+
+        public int EntityCount { get { return miEntityCount; } }
+
+        protected override byte[] Build() { return null; }
+    }
+}
