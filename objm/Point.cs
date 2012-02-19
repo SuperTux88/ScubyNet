1a2c8e0dc6a147400983241ec40e8751888cf293
uditaren <uditaren@bitsetter.de>
2012-02-19 21:05:49 +0100

    distance plus

--- objm/Point.cs
+++ objm/Point.cs
@@ -9,26 +9,38 @@ namespace ScubyNet.obj
     {
         private double mPosX;
         private double mPosY;
-        private double mDirection;
-
+        
         public Point()
         {
         }
-
-        public Point(double vdPosX, double vdPosY, double vdDirection)
+		
+        public Point(double vdPosX, double vdPosY)
         {
             mPosX      = vdPosX;
             mPosY      = vdPosY;
-            mDirection = vdDirection;
         }
 
-        public double getDistanceTo(Point voPoint){
+        public double getDistanceTo(Point voPoint) {
             return Math.Sqrt(
                        Math.Pow((voPoint.PosX - this.PosX), 2)
                      + Math.Pow((voPoint.PosY - this.PosY), 2)
                    );
         }
+		
+		public double getShortestDistanceTo(Point voPoint) {
+			double x = (this.PosX < 500)? 1000:-1000;
+			double y = (this.PosY < 500)? 1000:-1000;
+			Point p2 = new Point(voPoint.PosX + x, voPoint.PosY); 
+			Point p3 = new Point(voPoint.PosX, voPoint.PosY + y); 
+			Point p4 = new Point(voPoint.PosX + x, voPoint.PosY + y); 			
+			double dist = getDistanceTo(voPoint);
+			dist = Math.Min(dist, getDistanceTo(p2));
+			dist = Math.Min(dist, getDistanceTo(p3));
+			dist = Math.Min(dist, getDistanceTo(p4));
+			return dist;
+		}
 
+		
         public double getAngle(Point voPoint)
         {
             return Math.Atan2(
@@ -39,6 +51,5 @@ namespace ScubyNet.obj
 
         public double PosX { get { return mPosX; } set { mPosX = value; } }
         public double PosY { get { return mPosY; } set { mPosY = value; } }
-        public double Direction { get { return mDirection; } set { mPosX = mDirection; } }
     }
 }
